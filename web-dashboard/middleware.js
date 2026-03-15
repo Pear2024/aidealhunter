39b8296f9f93afb28365601a5e3706b4542eb669
Pear2024 <155395970+Pear2024@users.noreply.github.com>
2026-03-14 19:58:33 -0700

    fix: Webhooks intercepted by Clerk Dev Config

--- web-dashboard/middleware.js
+++ web-dashboard/middleware.js
@@ -4,9 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Skip Next.js internals, static files, and backend bot routes
+    '/((?!_next|api/webhooks|api/cron|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes except webhooks and cron
+    '/(api/(?!webhooks|cron).*|trpc.*)',
   ],
 };
