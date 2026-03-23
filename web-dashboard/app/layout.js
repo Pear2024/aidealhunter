f8458d2d09acf449ef369b2569caf0e5c91a22bf
Pear2024 <155395970+Pear2024@users.noreply.github.com>
2026-03-23 06:46:27 -0700

    Force GTM script to render statically via dangerouslySetInnerHTML to bypass Next.js 14 App Router hydration obfuscation, allowing Google Tag Manager bots to verify installation successfully.

--- web-dashboard/app/layout.js
+++ web-dashboard/app/layout.js
@@ -24,15 +24,17 @@ export default function RootLayout({ children }) {
       <html lang="en">
         <head>
           {/* Google Tag Manager */}
-          <Script id="google-tag-manager" strategy="beforeInteractive">
-            {`
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-N9ZWGK5G');
-            `}
-          </Script>
+              `
+            }}
+          />
           {/* End Google Tag Manager */}
           <script 
               async 
